920f2f1717f25fe8c84119f5a888c0b7acd9c045
krystal <krystalcl001@gmail.com>
2017-03-22 22:30:47 +0800

    异步加载数据

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -1,6 +1,7 @@
 (function(angular) {
 	'use strict';
 
+
 	var module = angular.module('moviecat.in_theaters', ['ngRoute']);
 
 	module.config(['$routeProvider', function($routeProvider) {
@@ -10,8 +11,24 @@
 		});
 	}]);
 
-	module.controller('InTheatersController', ['$scope',function($scope) {
+	module.controller('InTheatersController', [
+		'$scope',
+		'$http',
+		function($scope,$http) {
 
+			$scope.subjects = [];
+			$scope.message = '';
+			$http.get('/app/data.json').then(function(res){
+				if(res.status==200){
+					$scope.subjects = res.data.subjects;
+				}else{
+					$scope.message="获取数据错误,错误信息："+res.statusText;
+				}
+
+				// console.log(data);
+			},function(err){
+				$scope.message="获取数据错误,错误信息："+err.statusText;
+			});
 	}]);
 
 })(angular);
